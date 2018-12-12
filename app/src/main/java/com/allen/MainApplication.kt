4ea3e4f699a4fb2e2d5a8034bdfe46228bd97051
csgsky <674143140@qq.com>
2018-12-13 00:26:55 +0800

    testmerge

--- app/src/main/java/com/allen/MainApplication.kt
+++ app/src/main/java/com/allen/MainApplication.kt
@@ -1,11 +1,12 @@
 package com.allen
 
 import android.app.Application
+import android.util.Log
 
 class MainApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-
+        Log.i(MainApplication::class.simpleName, "test")
     }
 }
